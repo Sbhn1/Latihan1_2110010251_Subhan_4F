65d53ceaca0d9f741aad4a415330269be4ceb544
Sbhn1 <subhansyn@gmail.com>
2023-06-13 00:38:09 +0700

    Melengkapi method matematika

--- src/Hitung/Matematika.java
+++ src/Hitung/Matematika.java
@@ -13,4 +13,16 @@ public Matematika (double bil1,double bil2){
   public double setPenjumlahan(){
      return bil1 + bil2;
  }
+   //Membuat method setPengurangan
+  public double setPenguranan(){
+     return bil1 - bil2;
+ }
+   //Membuat method setPerkalian
+  public double setPerkalian(){
+     return bil1 * bil2;
+ }
+   //Membuat method setPembagian
+  public double setPembagian(){
+     return bil1 / bil2;
+ }
 }
